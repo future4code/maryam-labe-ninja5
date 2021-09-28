a48822c2c0b2436e5cc78382aa484182fcb0ec61
Gabriel Luiz Gonçalves <blgoncalves@hotmail.com>
2021-09-28 10:27:03 -0300

    Início do projeto

---
+++ src/components/Carrinho.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Carrinho extends React.Component {
+    render () {
+        return (
+            <div>
+                Tela do Carrinho
+            </div>
+        )
+    }
+}
+
+export default Carrinho
---
+++ src/components/Contrato.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Contrato extends React.Component {
+    render () {
+        return (
+            <div>
+                Tela de Contrato
+            </div>
+        )
+    }
+}
+
+export default Contrato
