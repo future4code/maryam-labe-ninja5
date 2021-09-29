9edd810e5ff4a30a389e9bfae43c7008b941df19
Gabriel Luiz Gonçalves <blgoncalves@hotmail.com>
2021-09-29 14:22:14 -0300

    Renderização dos serviços em cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,9 +10,9 @@ class Card extends React.Component {
     render () {
         return (
             <Cartao>
-                <h3>Nome</h3>
-                <h4>Preço</h4>
-                <h4>Prazo</h4>
+                <h3>{this.props.titulo}</h3>
+                <h4>{this.props.preco}</h4>
+                <h4>{this.props.prazo}</h4>
                 <button>Ver Detalhes</button>
                 <button>Adicionar no Carrinho</button>
             </Cartao>
